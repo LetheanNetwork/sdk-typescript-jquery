326f4d09a9621fffa57ba5bbdd745133e9fd8a10
Virgil <snider@host.uk.com>
2026-05-13 02:49:58 +0100

    feat: initial @lthn/sdk-typescript-jquery from openapi.yaml
    Generated from build/sdk/openapi.yaml via openapi-generator-cli
    (typescript-jquery). See parent repo for source: https://github.com/LetheanNetwork/desktop.
    
    Co-Authored-By: Virgil <virgil@lethean.io>

---
+++ api/api.ts
@@ -0,0 +1,5 @@
+export * from './RunnerApi';
+import { RunnerApi } from './RunnerApi';
+export * from './SystemApi';
+import { SystemApi } from './SystemApi';
+export const APIS = [RunnerApi, SystemApi];
